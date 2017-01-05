335ddb81bf3508492bb39e346dbd891d30db4f00
cewi@github.com <c.wichmann@gmx.de>
2017-01-05 22:23:26 +0000

    First commit

---
+++ src/PrintjobCreator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace cewi\tools;
+
+/**
+ * Description of PrintjobCreator
+ *
+ * @author cewi <c.wichmann@gmx.de>
+ */
+class PrintjobCreator
+{
+    public function helloworld()
+    {
+        echo 'Helloworld';
+    }
+
+}
