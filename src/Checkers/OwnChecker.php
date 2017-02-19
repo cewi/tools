e8378a79eecc61295aaecc72edbf70033174c2be
cewi@github.com <c.wichmann@gmx.de>
2017-02-19 09:51:24 +0000

    added ownChecker

---
+++ src/Checkers/OwnChecker.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Cewi\Checkers;
+
+/**
+ * Checks, if an Address can be delivered by ourselves
+ *
+ * @author cewi <c.wichmann@gmx.de>
+ * @license https://opensource.org/licenses/MIT
+ */
+class OwnChecker implements CheckerInterface
+{
+
+    /**
+     * type if address ist not deliverable
+     *
+     * @var string
+     */
+    protected $_type_error = 'Error';
+
+    /**
+     * how to deliver these envelopes
+     *
+     * @var string
+     */
+    protected $_type_success = 'Error';
+
+    /**
+     * cities where we deliver ourselves
+     *
+     * @var array
+     */
+    protected $_zipCities = [];
+
+    /**
+     * streets in thta cities
+     *
+     * @var array
+     */
+    protected $_zipStreets = [];
+
+    /**
+     *
+     * @var array
+     */
+    protected $_address = [];
+
+    /**
+     * set type for delivery
+     *
+     * @param array $options
+     */
+    public function __construct($options)
+    {
+        $this->_type_success = isset($options['type_success']) ? $options['type_success'] : 'Error';
+        $this->_type_error = isset($options['type_error']) ? $options['type_error'] : 'Error';
+
+        $this->_zipCities = isset($options['zipCities']) ? $options['zipCities'] : [];
+
+        $this->_zipStreets = isset($options['zipStreets']) ? $options['zipStreets'] : [];
+    }
+
+    /**
+     * check if address contains valid german postbox string
+     *
+     * @param array $address
+     * @return boolean
+     */
+    public function isDeliverable($address)
+    {
+        $this->_address = $address;
+        $this->_address['type'] = $this->_type_error;
+
+        if (in_array($this->_address['zip'], array_keys($this->_zipCities))) {
+
+            // we can deliver!
+            $city = $this->_generateNormalized($this->_address['city']);
+            $name = $this->_generateNormalized($this->_address['name']);
+
+            // is the found city ok?
+            if (preg_match('#' . $this->_zipCities[$this->_address['zip']]['pattern'] . '#i', $city, $matches)) {
+
+                // set id for city
+                $this->_address['city_id'] = $this->_zipCities[$this->_address['zip']]['id'];
+
+                $streets = $this->_zipStreets[$this->_address['zip']];
+
+                foreach ($streets as $street) {
+                    if (preg_match('#' . $street['pattern'] . '\s?(.*)?$#i', $name, $matches, PREG_OFFSET_CAPTURE)) {
+                        $this->_address['street_id'] = $street['id'];
+                        $this->_address['region_id'] = $street['region_id'];
+                        $this->_address['street'] = $street['name'];
+                        $this->_address['name'] = trim(substr($this->_address['name'], 0, $matches[0][1]));
+                        if (preg_match('#^(\d+)(.*)$#', $matches[1][0], $matches)) {
+                            $this->_address['number'] = (int) $matches[1];
+                            $this->_address['number_letter'] = trim($matches[2]);
+                        }
+                        $this->_address['type'] = $this->_type_success;
+                        return true;
+                    }
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * get changed Address
+     *
+     * @return array
+     */
+    public function getAddress()
+    {
+        return $this->_address;
+    }
+
+    /**
+     * normalizes Strings:
+     * - any type of whitspace to a single space
+     * - german umlauts to diphtongs
+     * - all lower case.
+     *
+     * @since 15.11.2016
+     *
+     * @param string $string String to normalize
+     *
+     * @return string normalized String
+     */
+    protected function _generateNormalized($string = null)
+    {
+        // change whitespace to single space letter
+        $stringWithoutSpaces = preg_replace('#[\s]#', ' ', $string);
+        // change german umlauts to diphtongs
+        $stringWithoutUmlauts = str_replace(['-', 'ß', 'ä', 'ö', 'ü', 'Ä', 'Ö', 'Ü', "'"], [' ', 'ss', 'ae', 'oe', 'ue', 'AE', 'OE', 'UE'], $stringWithoutSpaces);
+        // all to lowercase
+        $stringLowerCase = mb_strtolower($stringWithoutUmlauts, 'UTF-8');
+        // trim the string again
+        return trim($stringLowerCase);
+    }
+
+}
---
+++ tests/OwnCheckerTest.php
@@ -0,0 +1,181 @@
+<?php
+
+require_once __DIR__ . '/../../../autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use Cewi\Checkers\OwnChecker;
+
+/**
+ * PostboxCheckerTest
+ *
+ * @author cewi <c.wichmann@gmx.de>
+ */
+class OwnCheckerTest extends TestCase
+{
+
+    public $checker;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->checker = new OwnChecker([
+            'type_error' => 'Error',
+            'type_success' => 'Success',
+            'zipCities' => [
+                88888 => [
+                    'pattern' => 'musterstadt|klein(?: )?musterstadt',
+                    'id' => 1
+                ],
+                88889 => [
+                    'pattern' => 'musterheim|musterhausen',
+                    'id' => 2
+                ]
+            ],
+            'zipStreets' => [
+                88888 => [
+                    0 => [
+                        'pattern' => 'muster(?: )?strasse',
+                        'id' => 1,
+                        'region_id' => 1,
+                        'length' => 13,
+                        'name' => 'Musterstraße'
+                    ],
+                    1 => [
+                        'pattern' => 'master(?: )?strasse',
+                        'id' => 1,
+                        'region_id' => 1,
+                        'length' => 13,
+                        'name' => 'Musterstraße'
+                    ],
+                    2 => [
+                        'pattern' => 'am(?: )?weg',
+                        'id' => 2,
+                        'region_id' => 1,
+                        'length' => 6,
+                        'name' => 'Am Weg'
+                    ],
+                    3 => [
+                        'pattern' => 'im(?: )?weg',
+                        'id' => 2,
+                        'region_id' => 1,
+                        'length' => 6,
+                        'name' => 'Am Weg'
+                    ]
+                ],
+                88889 => [
+                    0 => [
+                        'pattern' => 'muster(?: )?weg',
+                        'id' => 3,
+                        'region_id' => 2,
+                        'length' => 11,
+                        'name' => 'Musterweg'
+                    ],
+                    1 => [
+                        'pattern' => 'master(?: )?weg',
+                        'id' => 3,
+                        'region_id' => 2,
+                        'length' => 11,
+                        'name' => 'Musterweg'
+                    ],
+                ],
+            ]
+        ]);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Test initial setup
+     *
+     * @return void
+     */
+    public function testInitialization()
+    {
+        $this->assertInstanceOf('Cewi\Checkers\OwnChecker', $this->checker);
+    }
+
+    /**
+     * address -check
+     */
+    public function testIsDeliverable()
+    {
+        $addresses = [
+            [
+                'name' => 'Erika Mustermann Musterstraße 88 // 2',
+                'zip' => 88888,
+                'city' => 'Musterstadt'
+            ],
+            [
+                'name' => 'Erika Mustermann Musterweg 8a',
+                'zip' => 88889,
+                'city' => 'Musterheim'
+            ],
+            [
+                'name' => 'Erika Mustermann Musterstraße 8',
+                'zip' => 88888,
+                'city' => 'Musterheim'
+            ],
+            [
+                'name' => 'Erika Mustermann NoStreet 1',
+                'zip' => 88888,
+                'city' => 'Musterstadt'
+            ],
+        ];
+        $expected = [
+            [
+                'type' => 'Success',
+                'name' => 'Erika Mustermann',
+                'street' => 'Musterstraße',
+                'street_id' => 1,
+                'region_id' => 1,
+                'number' => 88,
+                'number_letter' => '// 2',
+                'zip' => 88888,
+                'city' => 'Musterstadt',
+                'city_id' => 1
+            ],
+            [
+                'type' => 'Success',
+                'name' => 'Erika Mustermann',
+                'street' => 'Musterweg',
+                'street_id' => 3,
+                'region_id' => 2,
+                'number' => 8,
+                'number_letter' => 'a',
+                'zip' => 88889,
+                'city' => 'Musterheim',
+                'city_id' => 2
+            ],
+            false, // city and zip doesn't match
+            [
+                'type' => 'Error',
+                'name' => 'Erika Mustermann NoStreet 1',
+                'zip' => 88888,
+                'city' => 'Musterstadt',
+                'city_id' => 1
+            ], // street not found
+        ];
+        foreach ($addresses as $key => $address) {
+            if ($this->checker->isDeliverable($address)) {
+                $this->assertEquals($expected[$key], $this->checker->getAddress());
+            } else {
+                $this->assertFalse($expected[$key]);
+            }
+        }
+    }
+
+}
